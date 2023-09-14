b15ee09bca5dab27934e1238f32d4fa6ed1f55c7
Kenshin2438 <79082237+Kenshin2438@users.noreply.github.com>
2023-09-14 20:44:05 +0800

    Modified: - Renamed InfraredRay Mode - Delay checker (N cannt be ZERO)

--- SYSTEM/car.c
+++ SYSTEM/car.c
@@ -2,12 +2,26 @@
 
 #include "delay.h"
 
+#define IR_Left_Pin  GPIO_Pin_3
+#define IR_Right_Pin GPIO_Pin_4
+
+void CarCheck_Init(void) {  // Infrared Ray Mode
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
+  GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_IPU;
+  GPIO_InitStruct.GPIO_Pin   = IR_Left_Pin | IR_Right_Pin;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
 #define Motor_L_VCC_Pin GPIO_Pin_1
 #define Motor_L_GND_Pin GPIO_Pin_0
 #define Motor_R_VCC_Pin GPIO_Pin_7
 #define Motor_R_GND_Pin GPIO_Pin_6
 
-void CarMovement_Init(void) {
+void CarMovement_Init(void) {  // Motor Driver Mode
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
   GPIO_InitTypeDef GPIO_InitStruct;
@@ -28,72 +42,48 @@ void CarMovement_STOP(void) {
   Motor_Set_Low(Motor_R_VCC_Pin), Motor_Set_Low(Motor_R_GND_Pin);
 }
 
+#define PWM_LOOP 200
+
 void CarMovement_UP(int PWM) {
-  for (int _ = 0; _ < 150; _++) {
+  for (int _ = 0; _ < PWM_LOOP; _++) {
     Motor_Set_High(Motor_L_VCC_Pin), Motor_Set_Low(Motor_L_GND_Pin);
     Motor_Set_High(Motor_R_VCC_Pin), Motor_Set_Low(Motor_R_GND_Pin);
-    delay_us(PWM);
+    if (PWM > 0) delay_us(PWM);
     Motor_Set_Low(Motor_L_VCC_Pin), Motor_Set_Low(Motor_L_GND_Pin);
     Motor_Set_Low(Motor_R_VCC_Pin), Motor_Set_Low(Motor_R_GND_Pin);
-    delay_us(10 - PWM);
+    if (10 > PWM) delay_us(10 - PWM);
   }
 }
 
 void CarMovement_DOWN(int PWM) {
-  for (int _ = 0; _ < 150; _++) {
+  for (int _ = 0; _ < PWM_LOOP; _++) {
     Motor_Set_Low(Motor_L_VCC_Pin), Motor_Set_High(Motor_L_GND_Pin);
     Motor_Set_Low(Motor_R_VCC_Pin), Motor_Set_High(Motor_R_GND_Pin);
-    delay_us(PWM);
+    if (PWM > 0) delay_us(PWM);
     Motor_Set_Low(Motor_L_VCC_Pin), Motor_Set_Low(Motor_L_GND_Pin);
     Motor_Set_Low(Motor_R_VCC_Pin), Motor_Set_Low(Motor_R_GND_Pin);
-    delay_us(10 - PWM);
+    if (10 > PWM) delay_us(10 - PWM);
   }
 }
 
 void CarMovement_RIGHT(int PWM) {
-  for (int _ = 0; _ < 200; _++) {
+  for (int _ = 0; _ < PWM_LOOP; _++) {
     Motor_Set_High(Motor_L_VCC_Pin), Motor_Set_Low(Motor_L_GND_Pin);
     Motor_Set_Low(Motor_R_VCC_Pin), Motor_Set_High(Motor_R_GND_Pin);
-    delay_us(PWM);
+    if (PWM > 0) delay_us(PWM);
     Motor_Set_Low(Motor_L_VCC_Pin), Motor_Set_Low(Motor_L_GND_Pin);
     Motor_Set_Low(Motor_R_VCC_Pin), Motor_Set_Low(Motor_R_GND_Pin);
-    delay_us(10 - PWM);
+    if (10 > PWM) delay_us(10 - PWM);
   }
 }
 
 void CarMovement_LEFT(int PWM) {
-  for (int _ = 0; _ < 200; _++) {
+  for (int _ = 0; _ < PWM_LOOP; _++) {
     Motor_Set_Low(Motor_L_VCC_Pin), Motor_Set_High(Motor_L_GND_Pin);
     Motor_Set_High(Motor_R_VCC_Pin), Motor_Set_Low(Motor_R_GND_Pin);
-    delay_us(PWM);
+    if (PWM > 0) delay_us(PWM);
     Motor_Set_Low(Motor_L_VCC_Pin), Motor_Set_Low(Motor_L_GND_Pin);
     Motor_Set_Low(Motor_R_VCC_Pin), Motor_Set_Low(Motor_R_GND_Pin);
-    delay_us(10 - PWM);
+    if (10 > PWM) delay_us(10 - PWM);
   }
-}
-
-#undef Motor_Set_High
-#undef Motor_Set_Low
-
-void CarRed_Init(void) {
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
-  GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
-
-  GPIO_InitTypeDef GPIO_InitStruct;
-  GPIO_InitStruct.GPIO_Mode  = GPIO_Mode_IPU;
-  GPIO_InitStruct.GPIO_Pin   = GPIO_Pin_3 | GPIO_Pin_4;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOB, &GPIO_InitStruct);
-}
-
-/**
- * \brief 红外对管所面对的区域
- * \return 0: 白色，低电平，LED灯亮起
- *         1: 黑色，高电平，LED灯熄灭
- */
-int CarRed_Check_LEFT(void) {
-  return GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_3) != 0;
-}
-int CarRed_Check_RIGHT(void) {
-  return GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_4) != 0;
 }
--- SYSTEM/car.h
+++ SYSTEM/car.h
@@ -3,7 +3,7 @@
 
 #include "stm32f10x.h"
 
-/*****  电机驱动模块  *****/
+void CarCheck_Init(void);
 void CarMovement_Init(void);
 
 void CarMovement_STOP(void);
@@ -11,11 +11,5 @@ void CarMovement_UP(int);
 void CarMovement_DOWN(int);
 void CarMovement_RIGHT(int);
 void CarMovement_LEFT(int);
-
-/*****  红外模块  *****/
-void CarRed_Init(void);
-
-int CarRed_Check_LEFT(void);
-int CarRed_Check_RIGHT(void);
 
 #endif
